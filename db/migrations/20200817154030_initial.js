c247f0caa73dc60bb227be4e6fb98bc2aefacdb6
Brian Sunday <brians931@gmail.com>
2020-08-17 15:46:28 +0300

    setup

---
+++ db/migrations/20200817154030_initial.js
@@ -0,0 +1,9 @@
+const tableNames = require("../../src/constants/tableNames");
+
+exports.up = async (knex) => {
+  // make migrations
+};
+
+exports.down = async (knex) => {
+  //   drop tables
+};
---
+++ knexfile.js
@@ -0,0 +1,56 @@
+// Update with your config settings.
+
+require("dotenv").config();
+
+module.exports = {
+  // local: {
+  //   client: "sqlite3",
+  //   connection: {
+  //     filename: "./dev.sqlite3",
+  //   },
+  // },
+
+  development: {
+    client: "pg",
+    connection: {
+      database: process.env.POSTGRESS_DB,
+      user: process.env.POSTGRESS_USER,
+      password: process.env.POSTGRESS_PASSWORD,
+    },
+    migrations: {
+      directory: "./db/migrations",
+    },
+  },
+
+  // staging: {
+  //   client: "postgresql",
+  //   connection: {
+  //     database: "my_db",
+  //     user: "username",
+  //     password: "password",
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10,
+  //   },
+  //   migrations: {
+  //     tableName: "knex_migrations",
+  //   },
+  // },
+
+  // production: {
+  //   client: "postgresql",
+  //   connection: {
+  //     database: "my_db",
+  //     user: "username",
+  //     password: "password",
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10,
+  //   },
+  //   migrations: {
+  //     tableName: "knex_migrations",
+  //   },
+  // },
+};
