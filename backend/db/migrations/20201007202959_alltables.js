a401b3322df31c974c45d81efd787a9b4347ca92
Brian Sunday <brians931@gmail.com>
2020-10-07 21:06:58 +0300

    add new migration for the tables with fk references

--- backend/db/migrations/20201007202959_alltables.js
+++ backend/db/migrations/20201007202959_alltables.js
@@ -1,8 +1,91 @@
+const Knex = require("knex");
 
-exports.up = function(knex) {
-  
+const {
+  addDefaultColumns,
+  addNameDesc,
+  references,
+} = require("../../src/Utils/tableUtils");
+
+/** @param {Knex} knex */
+exports.up = async function (knex) {
+  // now for the tables with fk references
+
+  await knex.schema.createTable(tableNames.user, (table) => {
+    table.increments().notNullable();
+    table.string("fullname", 100).notNullable();
+    table.string("email", 254).notNullable().unique();
+    table.string("image_url", 2000);
+    table.boolean("active").notNullable().defaultTo(false);
+    references(table, tableNames.department);
+    addDefaultColumns(table);
+  });
+
+  await knex.schema.createTable(tableNames.auth, (table) => {
+    table.increments().notNullable();
+    references(table, tableNames.user);
+    table.string("password", 180).notNullable();
+    table.boolean("active").notNullable().defaultTo(false);
+    addDefaultColumns(table);
+  });
+
+  await knex.schema.createTable(tableNames.role_user, (table) => {
+    table.increments().notNullable();
+    references(table, tableNames.user);
+    references(table, tableNames.role);
+    addDefaultColumns(table);
+  });
+
+  await knex.schema.createTable(tableNames.ticket_type, (table) => {
+    addNameDesc(table);
+    references(table, tableNames.department);
+    addDefaultColumns(table);
+  });
+
+  await knex.schema.createTable(tableNames.ticket_subtype, (table) => {
+    addNameDesc(table);
+    references(table, tableNames.ticket_type);
+    addDefaultColumns(table);
+  });
+
+  await knex.schema.createTable(tableNames.ticket, (table) => {
+    table.increments().notNullable();
+    table.string("issue_summary", 600).notNullable();
+    table.string("description", 1300).notNullable();
+    references(table, table.user);
+    references(table, tableNames.ticket_subtype);
+    addDefaultColumns(table);
+  });
+
+  await knex.schema.createTable(tableNames.ticket_history, (table) => {
+    table.increments().notNullable();
+    references(table, table.user);
+    references(table, tableNames.status);
+    references(table, tableNames.ticket);
+    references(table, table.sla);
+    table.string("comment", 1500).notNullable();
+    addDefaultColumns(table);
+  });
+
+  await knex.schema.createTable(tableNames.resolution, (table) => {
+    table.increments().notNullable();
+    references(table, table.ticket_history);
+    table.string("description", 1500).notNullable();
+    addDefaultColumns(table);
+  });
 };
 
-exports.down = function(knex) {
-  
+/** @param {Knex} knex */
+exports.down = async function (knex) {
+  await Promise.all(
+    [
+      resolution,
+      ticket_history,
+      ticket,
+      ticket_subtype,
+      ticket_type,
+      role_user,
+      auth,
+      user,
+    ].map((tableName) => knex.schema.dropTable(tableName))
+  );
 };
