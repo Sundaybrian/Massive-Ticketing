21048c2b8057a0f6611aeaa652c5e9b8486049a3
Brian Sunday <brians931@gmail.com>
2020-10-13 13:37:18 +0300

    add default get route for the api

--- backend/src/api/index.js
+++ backend/src/api/index.js
@@ -3,6 +3,13 @@ const express = require("express");
 const status = require("./meta/status/status.routes");
 
 const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.json({
+    message: "Welcome to ticketing app api v1 ",
+  });
+});
+
 router.use("/status", status);
 
 module.exports = router;
