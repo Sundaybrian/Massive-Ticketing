be22d836525057a2f514470c3950bce37ffcbb4e
Brian Sunday <brians931@gmail.com>
2020-10-19 16:23:04 +0300

    create signup route

--- src/api/auth/auth.routes.js
+++ src/api/auth/auth.routes.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const Joi = require("joi");
 const User = require("../users/users.model");
 
 const { signupSchema } = require("./auth.validators");
@@ -11,5 +10,35 @@ router.post("/signin", signinSchema, signin);
 module.exports = router;
 
 async function signup(req, res, next) {
-  const { fullname, email, password, confirmPassword } = req.body;
+  const { fullname, email, password } = req.body;
+  try {
+    const existingUser = await User.query().where({ email }).first();
+    if (existingUser) {
+      const error = new Error("Email already in use");
+      res.status(403);
+      throw error;
+      //TODO MAYBE SEND EMAIL TO THE OWNER OF THE EMAIL
+    }
+
+    // create account
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const insertedUser = await User.query().insert({
+      fullname,
+      email,
+    });
+
+    // TODO insert to auth table too
+    const payload = {
+      ...insertedUser,
+    };
+
+    const token = await jwt.sign(payload);
+
+    res.status(201).json({
+      user: payload,
+      token,
+    });
+  } catch (error) {
+    next(error);
+  }
 }
