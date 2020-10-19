8dc16136e9c6a569cb8b448db8b1116c435434fe
Brian Sunday <brians931@gmail.com>
2020-10-19 16:30:11 +0300

    add jwt util

--- src/api/auth/auth.routes.js
+++ src/api/auth/auth.routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../users/users.model");
+const jwt = require("../../Utils/jwt");
 
 const { signupSchema } = require("./auth.validators");
 
