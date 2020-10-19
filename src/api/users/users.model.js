26ae274034362a12bc647c5c9f76635b98a31b15
Brian Sunday <brians931@gmail.com>
2020-10-19 16:57:43 +0300

    add relations between user and auth

--- src/api/users/users.model.js
+++ src/api/users/users.model.js
@@ -1,6 +1,7 @@
 const { Model } = require("objection");
 const tableNames = require("../../constants/tableNames");
 const userSchema = require("./users.schema.json");
+const Auth = require("../auth/auth.model");
 
 class User extends Model {
   static get tableName() {
@@ -9,6 +10,19 @@ class User extends Model {
 
   static get jsonSchema() {
     return userSchema;
+  }
+
+  static get relationMappings() {
+    return {
+      auth: {
+        relation: Model.HasOneRelation,
+        modelClass: Auth,
+        join: {
+          from: `${tableNames.user}.id`,
+          to: `${tableNames.auth}.user_id`,
+        },
+      },
+    };
   }
 }
 
