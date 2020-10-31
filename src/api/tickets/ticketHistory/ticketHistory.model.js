c6c4d66849161c821f5e11fb54ae7944bb0aeff7
Brian Sunday <brians931@gmail.com>
2020-10-31 20:34:18 +0300

    add ticket history model

---
+++ src/api/tickets/ticketHistory/ticketHistory.model.js
@@ -0,0 +1,57 @@
+const { Model } = require("objection");
+
+const schema = require("./ticketHistory.schema.json");
+const tableNames = require("../../../constants/tableNames");
+const User = require("../../users/users.model");
+const Ticket = require("../tickets.model");
+const Status = require("../../meta/status");
+
+class TicketHistory extends Model {
+    static get tableName() {
+        return tableNames.ticket_history;
+    }
+
+    static get jsonSchema() {
+        return schema;
+    }
+
+    static get relationMappings() {
+        return {
+            ticket: {
+                relation: Model.BelongsToOneRelation,
+                modelClass: Ticket,
+                join: {
+                    from: `${tableNames.ticket_history}.ticket_id`,
+                    to: `${tableNames.ticket}.id`,
+                },
+            },
+            staff: {
+                relation: Model.BelongsToOneRelation,
+                modelClass: User,
+                join: {
+                    from: `${tableNames.ticket_history}.assigned_staff_id`,
+                    to: `${tableNames.user}.id`,
+                },
+            },
+            status: {
+                relation: Model.BelongsToOneRelation,
+                modelClass: Status,
+                join: {
+                    from: `${tableNames.ticket_history}.status_id`,
+                    to: `${tableNames.status}.id`,
+                },
+            },
+
+            // sla: {
+            //     relation: Model.BelongsToOneRelation,
+            //     modelClass: Sla,
+            //     join: {
+            //         from: `${tableNames.ticket_history}.sla_id`,
+            //         to: `${tableNames.sla}.id`,
+            //     },
+            // },
+        };
+    }
+}
+
+module.exports = TicketHistory;
